7bf2849b4aaabac549cb8757c80ca0a4de01dadf
o3223558-design <o3223558@gmail.com>
2026-04-25 16:14:09 +0200

    Update 7éme scripte.ps1

--- 7éme scripte.ps1
+++ 7éme scripte.ps1
@@ -1,4 +1,4 @@
-#la il demande les information de l'utilisateur pour la suite 
+#il demande les information de l'utilisateur pour la suite 
 $coucher = Read-Host "A quelle heure t'es-tu couché ?"
 $reveil = Read-Host "A quelle heure t'es-tu réveillé ?"
 $age = Read-Host "Quel est ton âge ?"
@@ -8,15 +8,15 @@ $coucher = [int]$coucher
 $reveil = [int]$reveil
 $age = [int]$age
 
-# la c la parti ou il calcule le temps de someil 
+# la c'est la parti ou il calcule le temps de someil 
 if ($reveil -ge $coucher) {
 #	
     $sommeil = $reveil - $coucher
 #si l’heure de réveil est aprè ou égale à l’heure de coucher
-#23 - 21 = 2 heures de sommei
+#23 - 21 = 2 heures de sommeil
 } else {
     $sommeil = (24 - $coucher) + $reveil
-#en gros la nuit et cut en 2 parti, 24 - 23 = 1 heure et donc aprés 7 heures et hop 1 + 7 = 8 heures
+#La nuit et cut en 2 parti, 24 - 23 = 1 heure et donc aprés 7 heures et hop 1 + 7 = 8 heures
 }
 
 # la j'ai mis des valeur pour indiquer les "normes" pour que le message de fin ai un sense 
@@ -24,10 +24,10 @@ if ($age -ge 18) {
     $recommande = 8
 } else {
     $recommande = 9
-# je ne c pas si c les bonne valeur c qu'un exemple	
+# je ne c'est pas si c les bonne valeur c'est qu'un exemple	
 }
 
-# afiche si c une bonne nuit ou pas
+# affiche si c'est une bonne nuit ou pas
 if ($sommeil -ge ($recommande - 1) -and $sommeil -le ($recommande + 1)) {
     $etat = "bon sommeil"
 #se base donc sur les élément du dessus : l'age et le temps de someil	
@@ -35,12 +35,12 @@ if ($sommeil -ge ($recommande - 1) -and $sommeil -le ($recommande + 1)) {
     $etat = "pas obtimale"
 }
 
-# indique ou je v y mettre
+# indique ou je vais y mettre
 $bureau = "$env:USERPROFILE\Desktop\sommeil.txt"
 
 # sa met une ligne avec les info : heure , age , recommendé , et le temps de someil
 $contenu = "Sommeil : $sommeil h | Age : $age | Recommandé : $recommande h | Etat : $etat"
-# les pipe ( | ) sa permet d'avoir tt sur une seule ligne)
+# les pipe ( | ) sa permet d'avoir tout sur une seule ligne)
 
 #sa y met sur le bureau
 Add-Content $bureau $contenu
